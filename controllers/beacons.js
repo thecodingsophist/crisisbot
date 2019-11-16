747e56140b8a8a25ffb4a461c940f12d8dee5a05
Jayce Azua <jayce.azua@gmail.com>
2019-11-16 13:50:57 -0800

    create beacon

--- controllers/beacons.js
+++ controllers/beacons.js
@@ -8,7 +8,15 @@ const allBeacons = async (req, res) => {
 };
 // Create
 const addBeacon = async (req, res) => {
+  try {
+    const beacon = await new Beacon(req.body);
+    await beacon.save();
+    return res.status(200).send(beacon);
 
+  } catch (err) {
+
+    return res.status(500).send(err);
+  }
 };
 // Read
 const getBeacon = async (req, res) => {
