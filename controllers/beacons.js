bd45d3beb8359406255ed519d686410c9f5e10fe
Jayce Azua <jayce.azua@gmail.com>
2019-11-16 13:53:19 -0800

    get all beacons

--- controllers/beacons.js
+++ controllers/beacons.js
@@ -4,13 +4,16 @@ const {
 
 // Index
 const allBeacons = async (req, res) => {
-
+  const beacons = await Beacon.find()
+  beacons ? res.status(200).json(beacons) : res.status(500).json('Something went wrong.');
 };
+
 // Create
 const addBeacon = async (req, res) => {
   try {
     const beacon = await new Beacon(req.body);
     await beacon.save();
+
     return res.status(200).send(beacon);
 
   } catch (err) {
