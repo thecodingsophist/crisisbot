32c6cacf51d88262cd43d479698afdf40c226577
Jayce Azua <jayce.azua@gmail.com>
2019-11-16 13:09:38 -0800

    db connections

---
+++ dbConnections/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+// Set a Mongoose Promise Library
+mongoose.Promise = global.Promise;
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/crisis';
+mongoose.connect(dbURI, {
+  useNewUrlParser: true
+});
+mongoose.connection.once('open', () => {
+  console.log('Mongo Connected.')
+});
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false); // corrects the Deprecation Warning
+// mongoose.set('debug', true);
