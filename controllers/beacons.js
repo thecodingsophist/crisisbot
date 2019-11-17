685d936decbe4f1ca790cffb2732f807f0478418
Jayce Azua <jayce.azua@gmail.com>
2019-11-16 19:06:15 -0800

    ...

--- controllers/beacons.js
+++ controllers/beacons.js
@@ -13,10 +13,6 @@ const googleMapsClient = require('@google/maps').createClient({
 const allBeacons = async (req, res) => {
   const beacons = await Beacon.find()
 
-
-
-
-
   let city = beacons[0].city
   let state = beacons[0].state
   let address = beacons[0].street
@@ -25,7 +21,7 @@ const allBeacons = async (req, res) => {
     address: `${address}, ${city}, ${state}`
   }, function (err, response) {
     if (!err) {
-      // console.log("Help:", response.json.results)
+
       return res.status(200).json(response.json.results[0].geometry.viewport.northeast)
       // beacons ? res.status(200).json(response.json.results) : res.status(500).json('Something went wrong.');
 
@@ -65,6 +61,9 @@ const getBeacon = async (req, res) => {
   }
 };
 
+const messageBot = (req, res) => {
+  console.log(req.body)
+}
 
 module.exports = {
   allBeacons,
