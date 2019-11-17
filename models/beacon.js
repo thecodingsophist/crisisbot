0916efe996e9233d879f3516f91d1dba4e83fbc5
Jayce Azua <jayce.azua@gmail.com>
2019-11-16 16:52:15 -0800

    done...

--- models/beacon.js
+++ models/beacon.js
@@ -2,31 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BeaconSchema = new Schema({
-  first_name: {
-    type: String,
-    required: true
-  },
-  last_name: {
-    type: String
-  },
-  profile_pic: {
-    type: String,
-    required: true
-  },
+
   city: {
     type: String,
-    required: true
-  },
-  country: {
-    type: String,
-    required: true
-  },
-  latitude: {
-    type: Number,
-    required: true
-  },
-  longitude: {
-    type: Number,
     required: true
   },
   state: {
